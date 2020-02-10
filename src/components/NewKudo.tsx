1178c4faf568caef275bd21f8f34470cee6c7e7d
Jaroslav Šmolík <grissius@gmail.com>
2020-02-10 19:45:58 +0100

    ♻️ Refactpr form with formik

--- src/components/NewKudo.tsx
+++ src/components/NewKudo.tsx
@@ -1,69 +1,85 @@
 import React, { useState } from "react";
-import { FormControlLabel, Checkbox, FormControl, InputLabel, Select, MenuItem, TextField, Button, DialogContent, DialogTitle, Dialog, DialogActions } from "@material-ui/core";
+import { Button, DialogContent, DialogTitle, Dialog, DialogActions } from "@material-ui/core";
 import { SendOutlined } from "@material-ui/icons";
+import { Select } from 'material-ui-formik-components/Select'
+import { TextField } from 'material-ui-formik-components/TextField'
+import { Switch } from 'material-ui-formik-components/Switch'
 import { maxKudosPerPeriod } from "../config/config";
 import { State } from "../state/State";
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
 
 export const NewKudo = () => {
   const [opened, setOpened] = useState(false);
   let { loggedUser: user, users, createKudos } = State.useContainer()
   const close = () => setOpened(false);
-  const [message, setMessage] = useState('');
-  const [receiverUid, setReceiverUid] = useState('');
-  const [senderAgreesWithPublish, setSenderAgreesWithPublish] = useState(false);
-  const [senderAnonymous, setSenderAnonymous] = useState(true);
-
-  const handleOk = async () => {
-    const kudoData = {
-      message: message,
-      receiverUid: receiverUid,
-      senderAgreesWithPublish: senderAgreesWithPublish,
-      senderAnonymous: senderAnonymous
-    };
-    createKudos(kudoData);
-    close();
-  };
 
   if (!user) return null;
   return (
     <div>
-      {user && (
-        <Button size="large" variant="contained" color="primary" onClick={() => setOpened(true)} startIcon={<SendOutlined />}>
-          Send Kudos ({user.kudosSentThisPeriod}/{maxKudosPerPeriod})
+      <Button size="large" variant="contained" color="primary" onClick={() => setOpened(true)} startIcon={<SendOutlined />}>
+        Send Kudos ({user.kudosSentThisPeriod}/{maxKudosPerPeriod})
         </Button>
-      )}
       <Dialog open={opened} onClose={close}>
-        <DialogTitle id="form-dialog-title">Send Kudos</DialogTitle>
-        <DialogContent className="kudo-from">
-          <FormControl>
-            <InputLabel>Your hero</InputLabel>
-            <Select onChange={e => setReceiverUid(String(e.target.value))}>
-              {users.map(u => <MenuItem value={u.uid}>{`${u.displayName} (${u.email})`}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <TextField
-            label="Message"
-            multiline
-            rowsMax="4"
-            onChange={e => setMessage(e.target.value)}
-          />
-          <FormControlLabel
-            control={<Checkbox onChange={e => setSenderAgreesWithPublish(e.target.checked)} value="jason" />}
-            label="Agree to publish"
-          />
-          <FormControlLabel
-            control={<Checkbox onChange={e => setSenderAnonymous(!e.target.checked)} value="jason" />}
-            label="Agree to display my name"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={close} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleOk} variant="contained" color="primary">
-            Send
-          </Button>
-        </DialogActions>
+        <Formik initialValues={{
+          receiverUid: '',
+          message: '',
+          senderAgreesWithPublish: false,
+          senderAnonymous: false,
+        }}
+          validationSchema={
+            Yup.object().shape({
+              receiverUid: Yup.string()
+                .notOneOf([user.uid], 'Rude to kudos oneself')
+                .required(),
+              message: Yup.string()
+                .min(1)
+                .required(),
+            })
+          }
+          onSubmit={async values => {
+            await createKudos(values);
+            close();
+          }}>
+          <Form>
+            <DialogTitle id="form-dialog-title">Send Kudos</DialogTitle>
+            <DialogContent className="kudo-from">
+              <Field
+                required
+                name="receiverUid"
+                label="ReceiverUid"
+                options={users.map(u => ({ value: u.uid, label: `${u.displayName} (${u.email})` }))}
+                component={Select}
+              />
+              <Field
+                required
+                name="message"
+                label="Message"
+                component={TextField}
+              />
+              <Field
+                required
+                name="senderAgreesWithPublish"
+                label="SenderAgreesWithPublish"
+                component={Switch}
+              />
+              <Field
+                required
+                name="senderAnonymous"
+                label="senderAnonymous"
+                component={Switch}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={close} color="primary">
+                Cancel
+              </Button>
+              <Button type="submit" variant="contained" color="primary">
+                Send
+              </Button>
+            </DialogActions>
+          </Form>
+        </Formik>
       </Dialog>
     </div>
   );
